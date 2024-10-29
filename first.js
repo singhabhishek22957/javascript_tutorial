0dae9bdd6f0cd12e61c5f113a7ec3e747281a0f4
singhabhishek22957 <singhabhishek22957@gmai.com>
2024-10-29 20:24:21 +0530

    this the javascript code for revise

---
+++ first.js
@@ -0,0 +1,13 @@
+console.log("Abhishek Kumar singh ");
+const pi = 3.14
+let var1 = 30;
+var var2 = 40;
+
+console.log(var2);
+var var2 = 24; 
+console.log(var2);
+// let var1 = 400;   it gives error because let variable can't redeclare within a same scope  but it can be update
+console.log(var1);
+
+
+
